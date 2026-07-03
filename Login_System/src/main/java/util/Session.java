13739b9732066971bac73dead67535982d17d130
fkfkfk112233 <kk60607kk@gmail.com>
2026-07-03 10:59:24 +0800

    Create Session.java

---
+++ Login_System/src/main/java/util/Session.java
@@ -0,0 +1,33 @@
+package util;
+
+import model.User;
+
+public class Session {
+
+	private static User currentUser;
+	
+	/**
+	 *登入 
+	 */
+	public static void login(User user)
+	{
+		currentUser = user;
+	}
+	
+	/**
+	 * 登出
+	 */
+	public static void logout()
+	{
+		currentUser = null;
+	}
+	
+	/**
+	 * 是否已登入
+	 */
+	public static boolean isLogin()
+	{
+		return currentUser != null;
+	}
+	
+}
