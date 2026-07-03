bcde4bfe18a3e2ff3fe3eca5de947732ddc08308
fkfkfk112233 <kk60607kk@gmail.com>
2026-07-03 10:59:27 +0800

    Update TestAuthService.java

--- Login_System/src/main/java/test/TestAuthService.java
+++ Login_System/src/main/java/test/TestAuthService.java
@@ -3,6 +3,7 @@ package test;
 import model.User;
 import service.AuthService;
 import service.impl.AuthServiceImpl;
+import util.Session;
 
 public class TestAuthService {
 
@@ -14,10 +15,19 @@ public class TestAuthService {
 
         if (user != null) {
             System.out.println("登入成功");
+            
+            System.out.println(Session.isLogin());
+            
             System.out.println(user.getName());
         } else {
             System.out.println("登入失敗");
         }
+        
+        System.out.println("================================");
+        
+        Session.logout();
+
+        System.out.println(Session.isLogin());
 
 	}
 
