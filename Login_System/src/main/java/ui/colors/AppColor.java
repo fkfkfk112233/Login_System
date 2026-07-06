3334424826cc24cb091b519a17d0ba248600738b
fkfkfk112233 <kk60607kk@gmail.com>
2026-07-06 12:22:52 +0800

    Create AppColor.java

---
+++ Login_System/src/main/java/ui/colors/AppColor.java
@@ -0,0 +1,31 @@
+package ui.colors;
+
+import java.awt.Color;
+
+public final class AppColor {
+
+    //========================
+    // Message
+    //========================
+	
+	//錯誤
+	public static final Color ERROR =
+			Color.RED;
+	
+	//成功
+	public static final Color SUCCESS =
+			Color.GREEN;
+	
+	//警告
+	public static final Color WARNING =
+			Color.YELLOW;
+	
+	//提示
+	public static final Color INFO =
+			new Color(23, 162, 184);
+	
+	//一般
+    public static final Color NORMAL =
+            Color.BLACK;
+    
+}
